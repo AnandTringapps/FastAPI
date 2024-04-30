1782010724d2d52a2c1e31d4b08d2ffbf134261f
AnandTringapps <129666267+AnandTringapps@users.noreply.github.com>
2024-04-30 16:14:18 +0530

    Added HTTP Methods

--- app/main.py
+++ app/main.py
@@ -1,8 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Body
 
 app = FastAPI()
+
+Employee_data=[
+    
+    {'id': 1,'name': 'Anand', 'age': 24},
+    {'id': 2,'name': 'sandhya', 'age': 22},
+    {'id': 3,'name': 'surya', 'age': 23}
+]
 
 @app.get("/hello")
 def hello():
     return {"message": "Hello World"}
+
+@app.get("/all")
+async def get_all_employees():
+    return Employee_data
+@app.get("/{id}")
+async def get_employee(id: int):
+    return Employee_data[id-1]
+
+@app.put("/update/{employee_id}")
+async def update_employee(employee_id: int, updated_data: dict):
+    for i, employee in enumerate(Employee_data):
+        if employee["id"] == employee_id:
+            employee.update(updated_data)
+            return {"message": "Employee updated successfully"}
+    return {"message": "Employee not found"}
+
+@app.post("/insert")
+async def add_employee(new_employee: dict = Body(...)):
+    new_id = max(employee_id["id"] for employee_id in Employee_data) + 1
+    new_employee["id"] = new_id
+    Employee_data.append(new_employee)
+
+    return {"message": "Employee added successfully", "new_employee_id": new_id}
+
+
+@app.delete("/delete/{employee_id}")
+async def delete_employee(employee_id: int):
+    for i, employee in enumerate(Employee_data):
+        if employee["id"] == employee_id:
+            del Employee_data[i]  
+            return {"message": "Employee deleted successfully"}
+    return {"message": "Employee not found"}
+
 
